88687ffc27d67c73e456e228b9aac9a9e2396a67
saran viswa <bsaranviswa@gmail.com>
2022-06-17 11:56:08 +0530

    new commit

--- MenuItemWebApi/WeatherForecast.cs
+++ MenuItemWebApi/WeatherForecast.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace MenuItemWebApi
 {
     public class WeatherForecast
@@ -8,6 +10,6 @@ namespace MenuItemWebApi
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
+        public string Summary { get; set; }
     }
 }
